152906fd56f9b3e5d8051727ba8ef421680426e0
ketantb <kbhatkhore777@gmail.com>
2023-04-19 12:44:15 +0530

    .env removed

--- server/router/listproperties-cart.js
+++ server/router/listproperties-cart.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Razorpay = require("razorpay");
-<<<<<<<< HEAD:server/router/listproperties-cart.js
 const HotelBook = require("../models/post-property");
-========
-const PostProperty = require("../models/hotelbook");
->>>>>>>> c709f4b53e0b6067e5c4455df0673bd208cf9508:server/router/post-property.js
 const cartModel = require("../models/cart");
 require("dotenv").config();
 
@@ -19,7 +15,7 @@ const instance = new Razorpay({
 
 router.post("/hotelbook", async (req, res) => {
   try {
-    const hotelBook = await PostProperty.create(req.body);
+    const hotelBook = await HotelBook.create(req.body);
     res.status(200).json(hotelBook);
   } catch (error) {
     console.log(error.message);
@@ -29,7 +25,7 @@ router.post("/hotelbook", async (req, res) => {
 
 router.get("/hotelbook", async (req, res) => {
   try {
-    const hotelBook = await PostProperty.find({});
+    const hotelBook = await HotelBook.find({});
     res.status(200).json(hotelBook);
   } catch (error) {
     res.status(5009).json({ message: error.message });
@@ -40,7 +36,7 @@ router.get("/hotelbook", async (req, res) => {
 router.get("/hotelBook/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const hotelBook = await PostProperty.findById(id);
+    const hotelBook = await HotelBook.findById(id);
     res.status(200).json(hotelBook);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -51,14 +47,14 @@ router.get("/hotelBook/:id", async (req, res) => {
 router.put("/hotelbook/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const hotelBook = await PostProperty.findByIdAndUpdate(id, req.body);
+    const hotelBook = await HotelBook.findByIdAndUpdate(id, req.body);
     //we cannot find any product in database
     if (!hotelBook) {
       return res
         .status(404)
         .json({ message: `cannot find any hotel Book with ${id}` });
     }
-    const updatedHotelBook = await PostProperty.findById(id);
+    const updatedHotelBook = await HotelBook.findById(id);
     res.status(200).json(updatedHotelBook);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -69,7 +65,7 @@ router.put("/hotelbook/:id", async (req, res) => {
 router.delete("/hotelbook/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const hotelBook = await PostProperty.findByIdAndDelete(id, req.body);
+    const hotelBook = await HotelBook.findByIdAndDelete(id, req.body);
     //we cannot find any product in database
     if (!hotelBook) {
       return res
