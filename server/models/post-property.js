689e20e2d87545af1f7c78f781fe76c3de2ced9f
Harshada0611 <harshadaa1997@gmail>
2023-04-20 14:06:19 +0530

    added property schema

--- server/models/post-property.js
+++ server/models/post-property.js
@@ -1,18 +1,72 @@
 const mongoose = require("mongoose");
 
 const PostPropertySchema = mongoose.Schema({
-  resortImage:
-    { type: String }
-  ,
-  resortName: {
-    type: String,
-  },
-  location: {
-    type: String,
-  },
-  description: {
-    type: String,
-  }
+
+    resortName: {
+        type: String,
+    },
+    resortLocation: {
+        type: String,
+    },
+    resortDescription: {
+        type: String,
+    },
+    rooms: [
+        {
+            imgurl: [
+                { type: String },
+            ],
+            roomType: {
+                type: String
+            },
+            totalRooms: {
+                type: Number
+            },
+            availableroom: {
+                type: Number
+            },
+            ratePerNight: {
+                type: Number,
+            },
+            aminities: [
+
+            ],
+
+
+
+            //aminities
+            breakfast: {
+                type: Boolean
+            },
+            mosquitonet: {
+                type: Boolean,
+            },
+            Wifi: {
+                type: Boolean,
+            },
+            coldshower_24hrs: {
+                type: Boolean,
+            },
+            airconditioned: {
+                type: Boolean,
+            },
+            roomService: {
+                type: Boolean,
+            },
+            seaView: {
+                type: Boolean
+            },
+            swimmingPool: {
+                type: Boolean
+            },
+            kitchenFacilities: {
+                type: Boolean
+            },
+            spa: {
+                type: Boolean
+            }
+        },
+    ],
 });
 
 const PostProperty = mongoose.model("PostProperty", PostPropertySchema);
